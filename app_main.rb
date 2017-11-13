68c24af61ccb2942ee1b92115e24cf9fc54c2db9
Aoi_Avant <ytoy.aoi@gmail.com>
2017-11-13 06:49:20 +0000

    change app_main

--- app_main.rb
+++ app_main.rb
@@ -27,13 +27,16 @@ post '/callback' do
       case event.type
       when Line::Bot::Event::MessageType::Text
         message = [
-            {type: 'text',text: "愚かなる人類よ\n貴様の汚れた言葉をここに繰り返そう"},
+            {type: 'text',text: "愚かなる人類よ\n高貴な私が貴様の言葉を繰り返してやろう"},
             {type: 'text',text: event.message['text']},
-            {type: 'text',text: "魂に刻み込め"}
+            {type: 'text',text: "この喜びを魂に刻み込め"}
         ]
         client.reply_message(event['replyToken'], message)
       else
-        message = {type: 'text',text: "愚かなる人類よ\n貴様の言葉で喋れ"} 
+        message = [
+            {type: 'text',text: "愚かなる人類よ\n言語すら自由に操れんのか"} ,
+            {type: 'text',text: "あまりにも哀れだ"}
+        ]
         client.reply_message(event['replyToken'], message)
       end
     end
